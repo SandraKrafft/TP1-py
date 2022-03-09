0a1a3b136191ec8fd33112276ed313e042b888af
Krafft <skrafft@et.esiea.fr>
2022-03-09 09:42:45 +0100

    Syracuse.py

--- Syracuse.py
+++ Syracuse.py
@@ -2,7 +2,7 @@ def syracuse(entier):
     while entier != 1:
         print(entier)
         if entier % 2 == 0:
-            entier = entier/2
+            entier = entier//2
         else:
             entier = entier*3+1
    
